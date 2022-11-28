a65664c39d068cda6c7799c965a2b9bb40c07b1b
Cody Palmer <codyryanpalmer@gmail.com>
2022-11-28 09:36:40 -0600

    Updates to springblog

--- src/main/java/com/example/springblog/controllers/PostController.java
+++ src/main/java/com/example/springblog/controllers/PostController.java
@@ -1,40 +1,39 @@
 package com.example.springblog.controllers;
+
 import com.example.springblog.models.Post;
 import com.example.springblog.models.User;
+import com.example.springblog.repositories.PostRepository;
 import com.example.springblog.repositories.UserRepository;
+import com.example.springblog.Services.AdService;
+import com.example.springblog.Services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import com.example.springblog.repositories.PostRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class PostController {
-    private final PostRepository postDao;
 
-    public PostController(PostRepository postsDao, UserRepository userDao){
-        this.postDao = postsDao;
+    private PostRepository postDao;
+
+    private UserRepository userDao;
+
+    private EmailService emailService;
+
+
+
+    public PostController(PostRepository postDao, UserRepository userDao, EmailService emailService) {
+        this.postDao = postDao;
         this.userDao = userDao;
+        this.emailService = emailService;
     }
-
-    private final UserRepository userDao;
 
     @GetMapping("/posts")
     public String postsIndex(Model model) {
         // Instantiate the list to hold all the Post objects
         List<Post> allPosts = postDao.findAll();
-        // Instantiate new Post objects
-//        Post p1 = new Post(1, "Test Title", "test body");
-//        Post p2 = new Post(2, "Exam Title", "exam body");
-//        Post p3 = new Post(3, "Assessment Title", "assessment body");
-//        Post p4 = new Post(4, "Final Title", "final body");
-        // Add newly instantiated Posts to the list
-//        allPosts.add(p1);
-//        allPosts.add(p2);
-//        allPosts.add(p3);
-//        allPosts.add(p4);
-        // Add the list of Posts as a model attribute to send to the template for rendering.
         model.addAttribute("allPosts", allPosts);
         // return the template
         return "posts/index";
@@ -43,21 +42,20 @@ public class PostController {
     @GetMapping("/posts/{id}")
     public String singlePost(@PathVariable long id, Model model) {
 //        Post singlePost = new Post(id, "First Post!", "This is the first time I've ever used Spring!");
-//        model.addAttribute("post", singlePost);
-        Post singlePost = postDao.getReferenceById(id);
+
+        Post singlePost = postDao.getById(id);
+        model.addAttribute("post", singlePost);
         return "posts/show";
     }
 
-    //    NOT USING FORM MODEL BINDING
+//    NOT USING FORM MODEL BINDING
 //    @GetMapping("/posts/create")
-//    @ResponseBody
-//    public String showCreateForm(Model model) {
+//    public String showCreateForm() {
 //        return "posts/create";
 //    }
 
-//    USING FORM MODEL BINDING
+    //    USING FORM MODEL BINDING
     @GetMapping("/posts/create")
-    @ResponseBody
     public String showCreateForm(Model model) {
         model.addAttribute("post", new Post());
         return "posts/create";
@@ -65,37 +63,43 @@ public class PostController {
 
 //    NOT USING FORM MODEL BINDING
 //    @PostMapping("/posts/create")
-//    @ResponseBody
 //    public String submitPost(@RequestParam(name = "title")String title, @RequestParam(name = "description")String description) {
 //        Post post = new Post();
 //        post.setTitle(title);
 //        post.setBody(description);
 //        User user = userDao.getById(1L);
+////        User user = userDao.findById(1L);
 //        post.setUser(user);
 //        postDao.save(post);
 //        return "redirect:/posts";
 //    }
 
-//    USING FORM MODEL BINDING
-    @PostMapping("/post/create")
+    //    USING FORM MODEL BINDING
+    @PostMapping("/posts/create")
     public String create(@ModelAttribute Post post){
         User user = userDao.getById(1L);
+//        User user = userDao.getReferenceById(1L);
         post.setUser(user);
         postDao.save(post);
+
+        emailService.prepareAndSend(post, "New Post Created!", "A new post has been created! Here is the title of your new post! Title: " + post.getTitle());
+
         return "redirect:/posts";
     }
 
-    @GetMapping("posts/{id}/edit")
-    public String editPosts(Model model){
-        model.addAttribute("post", new Post());
+
+    @GetMapping("/posts/{id}/edit")
+    public String editPost(@PathVariable long id, Model model){
+        model.addAttribute("post", postDao.getReferenceById(id));
         return "posts/editPost";
     }
 
-    @PostMapping("post/{id}/edit")
-    public String editPost(@PathVariable long id, @ModelAttribute Post post){
+    @PostMapping("/posts/edit")
+    public String editPost(@ModelAttribute Post post){
+        System.out.println(post.getId());
         User user = userDao.getById(1L);
         post.setUser(user);
-        post.setId(id);
+//        post.setId(id);
         postDao.save(post);
         return "redirect:/posts";
     }
